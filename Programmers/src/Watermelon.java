bb220d4509d71ca7f9c2c68d2602e3e435c3f52c
jja33705 <jja33705@gmail.com>
2020-11-25 14:35:31 +0900

    Wetermelon

---
+++ Programmers/src/Watermelon.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Watermelon {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		System.out.println(watermelon(n));
+	}
+	public static String watermelon(int n) {
+		String answer="";
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                answer+="¼ö";
+            }
+            else{
+                answer+="¹Ú";
+            }
+        }
+        return answer;
+	}
+
+}
