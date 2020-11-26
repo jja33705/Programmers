9be7d8ed98daabd9b9d302ff2399318e5879d17e
jja33705 <jja33705@gmail.com>
2020-11-26 15:44:31 +0900

    ????....

--- Programmers/src/noComplete.java
+++ Programmers/src/noComplete.java
@@ -7,14 +7,10 @@ public class noComplete {
 		System.out.println(solution(participant, completion));
 	}
 	public static String solution(String[] participant, String[] completion) {
-        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
-        for(int i=0;i<participant.length;i++){
-            if(i==participant.length-1){
-                answer=participant[participant.length-1];
-                break;
-            }
+        String answer = participant[participant.length-1];
+        for(int i=0;i<completion.length;i++){
             if(participant[i].equals(completion[i])==false){
                 answer=participant[i];
                 break;
