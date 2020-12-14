71eca61401d467a4aa9eba620ee6d7420e295808
jja33705 <jja33705@gmail.com>
2020-12-15 02:05:12 +0900

    D-1

---
+++ Programmers/src/StringDescendingOrder.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class StringDescendingOrder {
+
+	public static void main(String[] args) {
+		String a="Zbcdefg";
+		System.out.println(solution(a));
+	}
+	public static String solution(String s) {
+		String answer = "";
+        char[] arr=new char[s.length()];
+        for(int i=0;i<s.length();i++) {
+        	arr[i]=s.charAt(i);
+        }
+        Arrays.sort(arr);
+        for(int i=arr.length-1;i>=0;i--) {
+        	answer+=arr[i];
+        }
+        return answer;
+    }
+}
