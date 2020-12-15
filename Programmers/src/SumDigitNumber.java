55a794808b95dfc22c6451eb9736d72c0f32260a
jja33705 <jja33705@gmail.com>
2020-12-16 08:35:29 +0900

    D-day

---
+++ Programmers/src/SumDigitNumber.java
@@ -0,0 +1,14 @@
+public class SumDigitNumber {
+
+	public static void main(String[] args) {
+		System.out.println(solution(123456789));
+	}
+	public static int solution(int n) {
+        int answer = 0;
+        while(n!=0){
+            answer+=n%10;
+            n/=10;
+        }
+        return answer;
+    }
+}
