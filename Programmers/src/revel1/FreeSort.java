329c1c2ea7eaf966e9519baced80b2d37b61c614
jja33705 <jja33705@gmail.com>
2020-12-25 00:00:50 +0900

    StringClass

--- Programmers/src/revel1/FreeSort.java
+++ Programmers/src/revel1/FreeSort.java
@@ -5,7 +5,7 @@ public class FreeSort {
 		System.out.println(Arrays.toString(solution(new String[] {"sun","bed","car"},1)));
 		System.out.println(Arrays.toString(solution(new String[] {"abce","abcd","cdx"},2)));
 	}
-	public String[] solution(String[] strings, int n) {
+	public static String[] solution(String[] strings, int n) {
         for(int i=0;i<strings.length-1;i++) {
         	int min=i;
         	for(int j=i+1;j<strings.length;j++) {
